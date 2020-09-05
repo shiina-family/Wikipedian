7fcf2cf0b2806f7e447ffe9d0f40de8a56eed46a
Hirosuke <senolynn7584@gmail.com>
2020-09-05 16:01:47 +0900

    不安解消

--- extensions/commands.py
+++ extensions/commands.py
@@ -17,8 +17,9 @@ class Function(commands.Cog):
     @commands.command()
     async def wiki(self, ctx, title: str, location="ja"):
         locations = ["en", "ja"]
-        if location not in locations:
-            return
+        if location:
+            if location not in locations:
+                return
         r = requests.get(f"https://{location}.wikipedia.org/wiki/{title}")
         element = bs4.BeautifulSoup(r.text, "html.parser")
         element.find("img").extract()
