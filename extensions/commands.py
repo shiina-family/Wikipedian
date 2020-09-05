f1adebd997fa905455a17bd6f0f9fd211e8de880
Hirosuke <senolynn7584@gmail.com>
2020-09-05 19:27:12 +0900

    ぶっ飛びバグ修正

--- extensions/commands.py
+++ extensions/commands.py
@@ -21,12 +21,13 @@ class Function(commands.Cog):
         r = requests.get(f"https://{location}.wikipedia.org/wiki/{title}")
         element = bs4.BeautifulSoup(r.text, "html.parser")
         element.find("img").extract()
-        element.select("p", {"class":"mw-empty-elt"})[0].extract()
-        element.select("p", {"class":"mw-empty-elt"})[1].extract()
+        if location == "en":
+            element.select("p", {"class":"mw-empty-elt"})[0].extract()
+            element.select("p", {"class":"mw-empty-elt"})[1].extract()
         e = discord.Embed(title=f"__{element.h1.get_text()}__", description=urllib.parse.unquote(r.url))
         lendesc = len(element.select(".mw-parser-output > p")[0].get_text() + element.select(".mw-parser-output > p")[1].get_text())
         if(lendesc > 280):
-            e.set_footer(text=(element.select(".mw-parser-output > p")[0].get_text() + element.select(".mw-parser-output > p")[1].get_text())[:140-(lendesc+1)] + "...")
+            e.set_footer(text=(element.select(".mw-parser-output > p")[0].get_text() + element.select(".mw-parser-output > p")[1].get_text())[:280-(lendesc+1)] + "...")
         else:
             e.set_footer(text=element.select(".mw-parser-output > p")[0].get_text() + element.select(".mw-parser-output > p")[1].get_text())
         # e.set_thumbnail(url=element.find("img")["src"])
