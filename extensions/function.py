e5b2bac16ae548d28448fd8eb4a9d1e915830f2c
Hirosuke <senolynn7584@gmail.com>
2020-09-05 14:01:07 +0900

    #5

--- extensions/function.py
+++ extensions/function.py
@@ -17,7 +17,7 @@ class function(commands.Cog):
     @commands.command()
     async def wiki(self, ctx, title: str):
         r = requests.get("https://ja.wikipedia.org/wiki/"+title)
-        element = bs4.BeautifulSoup(r.text, "html-parser")
+        element = bs4.BeautifulSoup(r.text, "html.parser")
         e = discord.Embed(title=f"__{element.h1.get_text()}__", description=urllib.parse.unquote(r.url))
         e.set_footer(text=element.select(".mw-parser-output > p")[0].get_text())
         e.set_thumbnail(url=element.find(".mw-perser-output > img")[0]["src"])
