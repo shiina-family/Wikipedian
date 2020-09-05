7c7b894b89abbf87f931bc7d8256f14a88a356d0
Hirosuke <senolynn7584@gmail.com>
2020-09-05 21:45:42 +0900

    pタグが見つからなかった場合無視させる

--- extensions/commands.py
+++ extensions/commands.py
@@ -25,11 +25,14 @@ class Function(commands.Cog):
             element.select("p", {"class":"mw-empty-elt"})[0].extract()
             element.select("p", {"class":"mw-empty-elt"})[1].extract()
         e = discord.Embed(title=f"__{element.h1.get_text()}__", description=urllib.parse.unquote(r.url))
-        lendesc = len(element.select(".mw-parser-output > p")[0].get_text() + element.select(".mw-parser-output > p")[1].get_text())
-        if(lendesc > 280):
-            e.set_footer(text=(element.select(".mw-parser-output > p")[0].get_text() + element.select(".mw-parser-output > p")[1].get_text())[:280-(lendesc+1)] + "...")
-        else:
-            e.set_footer(text=element.select(".mw-parser-output > p")[0].get_text() + element.select(".mw-parser-output > p")[1].get_text())
+        try:
+            lendesc = len(element.select(".mw-parser-output > p")[0].get_text() + element.select(".mw-parser-output > p")[1].get_text())
+            if(lendesc > 280):
+                e.set_footer(text=(element.select(".mw-parser-output > p")[0].get_text() + element.select(".mw-parser-output > p")[1].get_text())[:280-(lendesc+1)] + "...")
+            else:
+                e.set_footer(text=element.select(".mw-parser-output > p")[0].get_text() + element.select(".mw-parser-output > p")[1].get_text())
+        except:
+            pass
         # e.set_thumbnail(url=element.find("img")["src"])
         await ctx.send(embed=e)
 
