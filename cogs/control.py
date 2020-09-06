b245b1c69415d45b0a81ac47cd344350071f0f35
fe-amazu <tenzyu.on@gmail.com>
2020-09-06 17:35:29 +0900

    unified the names of the files and cogs

--- cogs/control.py
+++ cogs/control.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 
 
-class Core(commands.Cog):
+class Control(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
@@ -28,4 +28,4 @@ class Core(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(Core(bot))
+    bot.add_cog(Control(bot))
