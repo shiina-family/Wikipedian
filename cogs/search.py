922720dd9b323be6c871fc2426605977e2a9943c
Hirosuke <senolynn7584@gmail.com>
2020-09-06 18:22:56 +0900

    ページ発見時の処理

--- cogs/search.py
+++ cogs/search.py
@@ -16,18 +16,26 @@ class Search(commands.Cog):
             f"https://ja.wikipedia.org/wiki/Special:Search?search={keyword}&go=Go&ns0=1")
         element = bs4.BeautifulSoup(r.text, "html.parser")
         results = []
+        check = []
+        c = element.find(".mw-parser-output > p")
+        check.append(c)
         for result in element.select(".mw-search-result-heading"):
             results.append("・" + result.get_text())
         if results:
-            e = discord.Embed(title="Search Result",
-                              description="\n".join(results[:15]))
+            e=discord.Embed(title="Search Result",
+                            description="\n".join(results[:15]))
             e.set_footer(
                 text=f"Total: {len(results)}(showing 15 of total items)")
         else:
-            e = discord.Embed(
-                title="Search Result",
-                description="result not found.")
-            e.set_footer(text="Total: 0")
+            if check:
+                e = discord.Embed(
+                    title="Search Result",
+                    description="Page found.\n"+urllib.parse.unquote(r.url))
+            else:
+                e = discord.Embed(
+                    title="Search Result",
+                    description="result not found.")
+                e.set_footer(text="Total: 0")
         await ctx.send(embed=e)
 
 
