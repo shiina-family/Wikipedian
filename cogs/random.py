04f438e22b3eb1b3fd4ef33e08e07f668d2df267
fe-amazu <tenzyu.on@gmail.com>
2020-09-07 19:05:54 +0900

    made seacher()

--- cogs/random.py
+++ cogs/random.py
@@ -9,6 +9,14 @@ class Random(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def seacher(self):
+        try:
+            title = wikipedia.random()
+            results = wikipedia.search(title)
+            return wikipedia.page(results[0])
+        except Exception:
+            print(results)
+            return self.seacher()
 
 def setup(bot):
     bot.add_cog(Random(bot))
